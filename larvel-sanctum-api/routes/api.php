c80be24a614b9c9a73c96a751e753f3090074c23
SC <chaolic6505@gmail.com>
2021-05-27 04:55:40 -0700

    restructured routes

--- larvel-sanctum-api/routes/api.php
+++ larvel-sanctum-api/routes/api.php
@@ -15,15 +15,20 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/:id', [ProductController::class, 'show']);
-// Route::post('/products', [ProductController::class, 'store']);
+// 
+// 
+// 
 
-Route::resource('products', ProductController::class);
+//Public Routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/:{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 //Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
